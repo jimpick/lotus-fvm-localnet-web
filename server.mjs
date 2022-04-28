b3b7c59ee8b85f88d7467686796b6a1c8e85def8
Jim Pick <jim@jimpick.com>
2022-04-27 23:38:54 -0700

    Use process.env.PORT to configure port

--- server.mjs
+++ server.mjs
@@ -8,9 +8,11 @@ fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
 
+const port = process.env.PORT || 3000
+
 const start = async () => {
   try {
-    await fastify.listen(3000, '0.0.0.0')
+    await fastify.listen(port, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)

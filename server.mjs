a43b9383cdae7a3ae39c7548ebe19bdb7c603295
Jim Pick <jim@jimpick.com>
2022-04-30 11:29:18 -0700

    Accept lib.rs via POST JSON body and compile
    Includes two test scripts.

--- server.mjs
+++ server.mjs
@@ -1,5 +1,12 @@
+import { promisify } from 'util' 
+import { exec } from 'child_process'
+import fs from 'fs'
+
 import Fastify from 'fastify'
 import fastifyCors from '@fastify/cors'
+import { encode } from 'borc'
+
+const execWithPromise = promisify(exec)
 
 const fastify = Fastify({
   logger: true
@@ -8,10 +15,51 @@ const fastify = Fastify({
 fastify.register(fastifyCors)
 
 fastify.post('/compile', async (request, reply) => {
-  return {
-    success: true,
-    binary: "xxx",
-    logs: "yyy"
+  // console.log('Request body', request.body)
+  if (!request.body || !request.body['lib.rs']) {
+    return (
+      reply
+        .code(400)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          error: 'Missing data for lib.rs'
+        })
+    )
+  }
+  fs.writeFileSync('fil-hello-world-actor/src/lib.rs', request.body['lib.rs'])
+  try {
+    const { stdout, stderr } = await execWithPromise(
+      'cargo build',
+      {
+        cwd: 'fil-hello-world-actor'
+      }
+    )
+    const buffer = fs.readFileSync(
+      'fil-hello-world-actor/target/debug/wbuild/' +
+      'fil_hello_world_actor/fil_hello_world_actor.compact.wasm'
+    )
+    const encoded = encode([ buffer ])
+    const base64Encoded = encoded.toString('base64')
+    return {
+      success: true,
+      logs: stderr,
+      wasmBinaryParamsEncoded: base64Encoded
+    }
+  } catch (e) {
+    console.error('Exception:', e.message)
+    console.error('Code:', e.code)
+    console.error('stdout:\n', e.stdout)
+    console.error('stderr:\n', e.stderr)
+    return (
+      reply
+        .code(400)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          success: false,
+          code: e.code,
+          error: e.stderr
+        })
+    )
   }
 })
 

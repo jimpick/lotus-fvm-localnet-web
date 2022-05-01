d952ee51b6a824519b950fc235d99ce1a32e4f4c
Jim Pick <jim@jimpick.com>
2022-04-30 21:19:39 -0700

    Try to get CORS headers right

--- test-curl-bad.sh
+++ test-curl-bad.sh
@@ -8,6 +8,6 @@ echo Patched code to cause error:
 diff -u fil-hello-world-actor/src/lib.rs tmp/lib.rs
 node -e 'd=require("fs").readFileSync("tmp/lib.rs", "utf8");console.log(JSON.stringify({ "lib.rs": d }))' > tmp/request.json
 set -x
-curl -v -X POST -H 'Content-Type: application/json' --data @tmp/request.json http://127.0.0.1:4000/compile > tmp/reply.json
+curl -v -H 'Content-Type: application/json' --data @tmp/request.json http://127.0.0.1:3000/compile > tmp/reply.json
 cat tmp/reply.json | jq '.. |= (if type == "string" then .[0:1000] else . end)'
 

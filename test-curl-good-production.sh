f56620058e6e97cf003f22c8e6a901a4c86412e9
Jim Pick <jim@jimpick.com>
2022-05-05 16:49:28 -0700

    Try adding JSON content-type header

--- test-curl-good-production.sh
+++ test-curl-good-production.sh
@@ -4,6 +4,6 @@
 mkdir -p tmp
 node -e 'd=require("fs").readFileSync("fil-hello-world-actor/src/lib.rs", "utf8");console.log(JSON.stringify({ "lib.rs": d }))' > tmp/request.json
 set -x
-curl -v -H 'Content-Type: application/json' --data @tmp/request.json https://fvm-1.default.knative.hex.camp/compile > tmp/reply.json
+curl -v -H 'Content-Type: application/json' --data @tmp/request.json https://fvm-2.default.knative.hex.camp/compile > tmp/reply.json
 cat tmp/reply.json | jq '.. |= (if type == "string" then .[0:1000] else . end)'
 
---
+++ test-curl-lotus-api-production.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#URL=http://127.0.0.1:3000
+URL=https://fvm-2.default.knative.hex.camp
+
+curl -v -X POST   -H "Content-Type: application/json"   --data '{ 
+      "jsonrpc": "2.0", 
+      "method": "Filecoin.Version", 
+      "params": [], 
+      "id": 1 
+    }'  $URL/rpc/v0 
+

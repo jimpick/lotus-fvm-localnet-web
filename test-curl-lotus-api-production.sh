380d7536f75543cc085f34bddaa4c3d5a0ab505d
Jim Pick <jim@jimpick.com>
2022-05-05 17:16:20 -0700

    Revert header_up

--- test-curl-lotus-api-production.sh
+++ test-curl-lotus-api-production.sh
@@ -3,6 +3,7 @@
 #URL=http://127.0.0.1:3000
 URL=https://fvm-2.default.knative.hex.camp
 
+set -x
 curl -v -X POST   -H "Content-Type: application/json"   --data '{ 
       "jsonrpc": "2.0", 
       "method": "Filecoin.Version", 

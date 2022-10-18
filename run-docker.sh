698dc0da28fb6f7bc4e8def19eb93e232e2ec1f2
Jim Pick <jim@jimpick.com>
2022-10-18 14:06:26 -0700

    Update lotus-fvm-localnet-ready

--- run-docker.sh
+++ run-docker.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-IMAGE=ghcr.io/jimpick/lotus-fvm-localnet-web-api@sha256:f7e8985827562cd1b499b6f9f1c3d313a963e5bee922a7c0f81d4eb12268c6af
+IMAGE=ghcr.io/jimpick/lotus-fvm-localnet-web-api@sha256:28cc112ce02d3d5c1ed7c47b80f2a9635c8b35205cf4a66a3328391d2e291d5d
 
 docker rm -f localnet-test
-#docker run -it -u 0 --entrypoint /bin/bash --name localnet-test $IMAGE
-docker run --detach --name localnet-test $IMAGE
+docker run -it -u 0 --entrypoint /bin/bash --name localnet-test $IMAGE
+#docker run --detach --name localnet-test $IMAGE
 echo docker exec -it localnet-test -- bash
